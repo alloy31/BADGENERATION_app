2e9e0dbdd3909882099dd478388313902aa963a5
Junhyun An <68099714+DDididingding@users.noreply.github.com>
2024-08-01 17:04:02 +0900

    Init : Create Project

---
+++ src/main/java/com/badgene/badgeneration_app/BadgenerationAppApplication.java
@@ -0,0 +1,13 @@
+package com.badgene.badgeneration_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BadgenerationAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BadgenerationAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/badgene/badgeneration_app/BadgenerationAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.badgene.badgeneration_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BadgenerationAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
